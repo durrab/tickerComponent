29fa6877f6cab14927f7f481d80e8ea0a4ecda4e
Durrab Khan <durrab@gmail.com>
2018-10-28 17:39:54 -0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*** src/index.js   ***/
+import React from 'react';
+import './styles.css';
+const TickerComponent = () => (
+    <h1>Hello from  Ticker Component</h1>
+);
+export default TickerComponent;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/*** webpack.config.js ***/
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, "tickerApp/src/index.html"),
+    filename: "./index.html"
+});
+module.exports = {
+    entry: path.join(__dirname, "tickerApp/src/index.js"),
+    output: {
+        path: path.join(__dirname, "tickerComponent/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [htmlWebpackPlugin],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    devServer: {
+        port: 3001
+    }
+};
